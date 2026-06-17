4bc8d84542503e7ca30d20248a173463480dcaa2
Marco Antonio Barón Effio <marcoantoniobaroneffio@MacBook-Air-de-Marco-2.local>
2026-06-16 23:41:21 -0500

    arreglo

--- src/main.ts
+++ src/main.ts
@@ -124,7 +124,8 @@ async function bootstrap() {
 
   // 📌 FALTA DOCUMENTAR
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  //await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log('🚀 PORT:', port);
   console.log('🚀 ENV PORT:', process.env.PORT)
